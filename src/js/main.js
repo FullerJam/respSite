6fa9a53ff20157042d286ec0c5a78d944d4ebb63
James Fuller <2fullj37@solent.ac.uk>
2018-01-11 16:22:34 +0000

    added image media queries for smaller image downloads, scss tidy, slight changes to html, code comments

--- src/js/main.js
+++ src/js/main.js
@@ -67,22 +67,20 @@ new Vue({
     },
 )
 
-const left = document.querySelector('.left')
-const right = document.querySelector('.right')
-const container = document.querySelector('.container')
+// Index page code below
+
+var left = document.querySelector('.left')
+var right = document.querySelector('.right')
+var container = document.querySelector('.container')
 
 left.addEventListener('mouseenter', () => {
-    container.classList.add('hover-left');
-});
+    container.classList.add('hover-left')})
 
 left.addEventListener('mouseleave', () => {
-    container.classList.remove('hover-left');
-});
+    container.classList.remove('hover-left')})
 
 right.addEventListener('mouseenter', () => {
-    container.classList.add('hover-right');
-});
+    container.classList.add('hover-right')})
 
 right.addEventListener('mouseleave', () => {
-    container.classList.remove('hover-right');
-});
+    container.classList.remove('hover-right')})
